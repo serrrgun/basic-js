f911ea576684035228a589fa51d167ea35fa44a3
serrrgun <serrrgun@mail.ru>
2020-02-23 16:37:25 +0300

    feat: add decision for task dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,14 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members)) {
+    let name = [];
+    for(let item of members) {
+      if (typeof item === 'string') {
+        const arrItem = item.trim().split('');
+        name.push(arrItem[0])
+      }
+    }
+    return name.sort().join('').toUpperCase()
+  } else {
+    return false
+  }
+}
