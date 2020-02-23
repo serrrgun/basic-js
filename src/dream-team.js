91d5becfb2f3a497d43f374db86b15e820389de0
serrrgun <serrrgun@mail.ru>
2020-02-23 17:08:01 +0300

    feat: add decision for task dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,13 +1,20 @@
 module.exports = function createDreamTeam(members) {
-  if (Array.isArray(members)) {
+// проверяем чтобы был массив
+if (Array.isArray(members)) {
+    // создаем пустой массив
     let name = [];
+    // проходимся циклом по входному массиву
     for(let item of members) {
+      // если элемент массива строка берем его
       if (typeof item === 'string') {
+        // делаем из элемента массив обрезая строку
         const arrItem = item.trim().split('');
-        name.push(arrItem[0])
+        // пушим в пустой созданный массив первый элемент приведенный к верхнему регистру
+        name.push(arrItem[0].toUpperCase());
       }
     }
-    return name.sort().join('').toUpperCase()
+    // возвращаем отсортированный по алфавиту массив приведенный к строке
+    return  name.sort().join('')
   } else {
     return false
   }
