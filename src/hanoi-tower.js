beea5beadc127fdc3b020a5f90bade30b21acc29
serrrgun <serrrgun@mail.ru>
2020-03-03 12:08:57 +0300

    feat: add decision hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,7 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    const result =  Math.pow(2, disksNumber);
+    return {
+        turns: result,
+        seconds:  result / (turnsSpeed / 3600)
+    }
 }
