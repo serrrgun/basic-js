653b23d9398130255bac3c774fb2877e73f0b69c
serrrgun <serrrgun@mail.ru>
2020-09-27 20:24:46 +0300

    feat: add desicion

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,8 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample( sampleActivity ) {
+  if (typeof(sampleActivity) != 'string' || sampleActivity == '' || !parseFloat(sampleActivity) || sampleActivity < 0) return false;
+  let result = Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / (0.693 / HALF_LIFE_PERIOD));
+  return (result > 0) ? result : false;
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  if (matrix === undefined || matrix.length === 0) {
+    return 0
+  } else {
+    const arr = matrix.reduce((acc, val) => acc.concat(val));
+    
+    let count = 0;
+    
+    arr.forEach(element => {
+      if (element === "^^") {
+        count++
+      }
+    });
+
+    return count;
+  }
 };
