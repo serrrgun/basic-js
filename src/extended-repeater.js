0c53ca7a73b41893288ebd3470f8c0a93b08fd72
serrrgun <serrrgun@mail.ru>
2021-03-04 16:39:23 +0300

    feat: add extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  const count = options.repeatTimes || 1;
+  const separator = options.separator || '+';
+  let addition = null;
+  const additionRepeatTimes = options.additionRepeatTimes || 1;
+  const additionSeparator = options.additionSeparator || '|'
+  
+  
+  if(options.addition === undefined) {
+    addition = '';
+  } else {
+    addition = options.addition + '';
+  }
+  const newAddition = `${addition}${additionSeparator}`.repeat(additionRepeatTimes).slice(0, -additionSeparator.length);
+  
+  console.log(newAddition)
+  const newStr = `${str}${newAddition}${separator}`.repeat(count).slice(0, -separator.length)
+  
+  return newStr
+  
 };
